41c9c8e12c0399e3f5e2790c198c63f77afe4032
James Tench <tenchjames@gmail.com>
2019-01-10 09:57:30 -0500

    removed markup to make basic boilerplate

--- src/App.js
+++ src/App.js
@@ -2,27 +2,10 @@ import React from "react";
 import { hot } from "react-hot-loader";
 
 class App extends React.Component {
-  state = {
-    count: 0
-  };
-
   render() {
-    const { count } = this.state;
     return (
       <div>
         <h1>Hello World!</h1>
-        <img src="./logo.png" alt="company logo" />
-        <h2 className={count > 10 ? "warning" : null}>Count: {count}</h2>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
-        >
-          +
-        </button>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
-        >
-          -
-        </button>
       </div>
     );
   }
---
+++ src/DefaultErrorBoundry.js
@@ -0,0 +1,21 @@
+import React from "react";
+import propTypes from "prop-types";
+
+export default class DefaultErrorBoundry extends React.Component {
+  state = {
+    isError: false
+  };
+
+  static propTypes = {
+    children: propTypes.node.isRequired
+  };
+
+  static getDerivedStateFromError() {
+    return { isError: true };
+  }
+  render() {
+    const { isError } = this.state;
+    const { children } = this.props;
+    return isError ? <div>Something went wrong!</div> : children;
+  }
+}
